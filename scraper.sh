3eac4f66eb4ce7da87f77b34c84ed63bcb8c5c6e
Maxence <maxencemousseaux.professional@gmail.com>
2025-04-06 11:14:55 +0200

    Add price and maj of scraper

--- scraper.sh
+++ scraper.sh
@@ -4,7 +4,7 @@
 URL="https://www.investing.com/currencies/usd-jpy"
 
 # Télécharger le HTML, extraire le prix et le sauvegarder dans price.txt
-curl -s $URL | grep -oP '"last":\K[\d.]+ ' | head -n 1 > price.txt
+curl -s $URL | grep -oP '"last":\K[\d.]+(?=\s)' > price.txt
 
 # Afficher le prix extrait
 cat price.txt
